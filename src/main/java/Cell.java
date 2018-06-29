6ee3b44b2e987647c64c449d124a290b538b66d9
sh3baka <napsterlv@gmail.com>
2018-06-29 15:13:06 +0300

    Refactoring

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,23 +1,34 @@
 public class Cell {
-    CellState state;
+    private CellState state;
+    public enum CellState {
+        ALIVE, DEAD
+    }
 
     public Cell(CellState state) {
         this.state = state;
+    }
+
+    public void update(int neighbors) {
+        if (isaDeadCell()) {
+            willBecomeAliveCheck(neighbors);
+        } else {
+            updateCellState(neighbors);
+        }
     }
 
     public CellState getState() {
         return state;
     }
 
-    public enum CellState {
-        ALIVE, DEAD
+    private void updateCellState(int neighbors) {
+        state = neighbors > 1 && neighbors < 4 ? CellState.ALIVE : CellState.DEAD;
     }
 
-    public void update(int i) {
-        if (state == CellState.DEAD) {
-            state =  i == 3 ? CellState.ALIVE : CellState.DEAD;
-        } else {
-            state = i > 1 && i < 4 ? CellState.ALIVE : CellState.DEAD;
-        }
+    private void willBecomeAliveCheck(int neighbors) {
+        state =  neighbors == 3 ? CellState.ALIVE : CellState.DEAD;
+    }
+
+    private boolean isaDeadCell() {
+        return state == CellState.DEAD;
     }
 }
