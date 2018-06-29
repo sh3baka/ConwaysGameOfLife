06545f667300dfe238f206d68fb6af8354b458c2
sh3baka <napsterlv@gmail.com>
2018-06-29 04:26:16 +0300

    First Commit

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,2 +1,15 @@
 public class Cell {
+    CellState state;
+
+    public Cell(CellState state) {
+        this.state = state;
+    }
+
+    public enum CellState {
+        ALIVE, DEAD
+    }
+
+    public CellState getNextState(int i) {
+        return CellState.DEAD;
+    }
 }
--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CellTest {
 
+    @Test
+    public void ShouldDieWithOnlyOneNeighbour() {
+        Cell cell = new Cell(Cell.CellState.ALIVE);
+
+        Cell.CellState actual = cell.getNextState(1);
+
+        assertEquals(Cell.CellState.DEAD, actual);
+    }
 }
